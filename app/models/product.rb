4c6b6ba25ebe4fa49bc44a376dcf7c19ce6f5e23
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2013-12-27 18:07:51 -0800

    Add Validation for title length.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, :length => {
+    :minimum => 10,
+    :message => 'must be at least ten characters long.'
+  } 
   validates :image_url, allow_blank: true, format: {
     with:    %r{\.(jpg|png)\Z}i,
     message: 'must be a URL for JPG or PNG image.'
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -10,7 +10,7 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
-  
+
   test "product price must be positive" do
     product = Product.new(title:       "My Book Title",
                           description: "yyy",
@@ -28,28 +28,25 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
   end
-  
+
   def new_product(image_url)
     Product.new(title:       "My Book Title",
                 description: "yyy",
                 price:       1,
                 image_url:   image_url)
   end
-
   test "image url" do
-    ok = %w{ natasha.jpeg natasha.png NATASHA.JPG NATASHA.Jpg
-             http://a.b.c/x/y/z/natasha.png }
-    bad = %w{ natasha.doc natasha.jpg/more natasha.jpeg.more }
-    
+    ok = %w{ natasha.jpg natasha.png NATASHA.JPG NATASHA.Jpg
+             http://a.b.c/x/y/z/natasha.jpg }
+    bad = %w{ natasha.doc natasha.jpg/more natasha.jpg.more }
     ok.each do |name|
       assert new_product(name).valid?, "#{name} should be valid"
     end
-
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
   end
-  
+
   test "product is not valid without a unique title" do
     product = Product.new(title:       products(:art).title,
                           description: "yyy", 
@@ -58,5 +55,24 @@ class ProductTest < ActiveSupport::TestCase
 
     assert product.invalid?
     assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:       products(:art).title,
+                          description: "yyy", 
+                          price:       1, 
+                          image_url:   "natasha.jpg")
+
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                 product.errors[:title]
+  end
+  
+  test "product title must be at least ten characters long" do
+    product = products(:art)
+    assert product.valid?, "product title shouldn't be invalid" 
+    
+    product.title = product.title.first(9)
+    assert product.invalid?, "product title shouldn't be valid"
   end
 end
