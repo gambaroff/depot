b15f5a95dca46b3f9d966daf26dc8b792caaff58
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2014-01-02 14:46:19 -0800

    Functional testing of store controller.

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.navi #ddtopmenubar a', minimum: 4
+    assert_select '.row .entry', 3
+    assert_select 'h3', 'Mr.President'
+    assert_select '.item-price', /\$[,\d]+\.\d\d/
   end
 
 end
